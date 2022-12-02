cfcec8175e8442af52b860f66818c7b31d868e84
Ed Street <edstreet@street-tek.com>
2022-12-02 01:01:02 -0500

    Autocommit from 2022-12-02 01:01:02
    Klipper on commit: e02b725
    
    Moonraker on commit: dde9bcc
    
    Mainsail version: v2.4.0

--- klipper_menuconfigs/klipper_make_octopus-466.sh
+++ klipper_menuconfigs/klipper_make_octopus-466.sh
@@ -1,4 +1,17 @@
 cd ~/klipper/
 make clean
 make menuconfig KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-octopus-446
-make KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-octopus-446
+
+echo -e 
+
+find /dev/serial/by-id/*
+
+read -p "Input flash device" flash_dev
+
+sudo service klipper stop
+
+make flash FLASH_DEVICE=$flash_dev KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-octopus-446
+
+sudo service klipper start
+
+
