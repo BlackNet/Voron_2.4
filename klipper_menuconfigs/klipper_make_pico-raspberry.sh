3d299799e82069c9f0dac674ca71d5a3c11f5801
Ed Street <edstreet@street-tek.com>
2023-01-03 23:30:29 -0500

    Autocommit from 2023-01-03 23:30:29
    Klipper on commit: 448c1a1
    
    Moonraker on commit: 225ec58
    
    Mainsail version: v2.4.1

---
+++ klipper_menuconfigs/klipper_make_pico-raspberry.sh
@@ -0,0 +1,9 @@
+cd ~/klipper/
+make clean KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-raspberry-pico
+make menuconfig KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-raspberry-pico
+make KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-raspberry-pico
+
+#sudo service klipper stop
+#make flash KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-raspberry-pico
+
+#sudo service klipper start
