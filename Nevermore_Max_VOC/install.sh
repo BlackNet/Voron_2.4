5acd1d423903376b20cc3a5578d3b42401dc48f9
Ed Street <ed@street-tek.com>
2023-03-13 00:40:53 -0400

    Autocommit from 2023-03-13 00:40:53
    Klipper on commit: 57c4da5
    
    Moonraker on commit: 80920dd
    
    Mainsail version: v2.4.1

---
+++ Nevermore_Max_VOC/install.sh
@@ -0,0 +1,2 @@
+ln -s ~/printer_data/config/Nevermore_Max_VOC/sgp40.py ~/klipper/klippy/extras/sgp40.py
+ln -s ~/printer_data/config/Nevermore_Max_VOC/voc_algorithm.py ~/klipper/klippy/extras/voc_algorithm.py
---
+++ Nevermore_Max_VOC/testing/go.sh
@@ -0,0 +1,6 @@
+joe mainsail/assets/index.244641ff.js
+
+joe fluidd/assets/index-a8560847.js
+
+sudo service nginx restart
+
