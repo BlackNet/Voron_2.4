e312d8847486775ee5ecbf456aede289b91f0b2d
Ed Street <ed@street-tek.com>
2023-03-15 16:55:01 -0400

    Autocommit from 2023-03-15 16:55:01
    Klipper on commit: 45dd30c
    
    Moonraker on commit: 80920dd
    
    Mainsail version: v2.5.0

---
+++ Nevermore_Max_VOC/testing/run_sensor.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+set -e
+source "./AdaFruit/bin/activate"
+python -u $1
--- Nevermore_Max_VOC/testing/sgp40-standalone.py
+++ Nevermore_Max_VOC/testing/sgp40-standalone.py
@@ -7,22 +7,24 @@ import board
 import adafruit_sgp40
 
 # If you have a temperature sensor, like the bme280, import that here as well
-#import adafruit_bme280
-import adafruit_si7021
+import adafruit_bme680
+#import adafruit_si7021
 
 i2c = board.I2C()  # uses board.SCL and board.SDA
 #i2c = board.STEMMA_I2C()  # For using the built-in STEMMA QT connector on a microcontroller
 sgp = adafruit_sgp40.SGP40(i2c)
 # And if you have a temp/humidity sensor, define the sensor here as well
 #bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
-si7021 = adafruit_si7021.SI7021(i2c)
-
+#si7021 = adafruit_si7021.SI7021(i2c)
+bme280 = adafruit_bme680.Adafruit_BME680_I2C(i2c)
 
 while True:
     print("Raw Gas: ", sgp.raw)
     # Lets quickly grab the humidity and temperature
-    temperature = si7021.temperature
-    humidity = si7021.relative_humidity
+    temperature = bme280.temperature
+#    temperature = si7021.temperature
+    humidity = bme280.relative_humidity
+#    humidity = si7021.relative_humidity
     compensated_raw_gas = sgp.measure_raw(temperature = temperature, relative_humidity = humidity)
     print("")
     time.sleep(1)
