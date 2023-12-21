8723e6660907466bf4e0d2dd5d8fd3262695552b
Ed Street <edstreet@street-tek.com>
2023-12-20 23:02:12 -0500

    Autocommit from 2023-12-20 23:02:12
    Klipper on commit: 3417940
    
    Moonraker on commit: 4235789
    
    Mainsail version: v2.9.0

---
+++ klipper_menuconfigs/klipper_make_mmb_can.sh
@@ -0,0 +1,35 @@
+cd ~/klipper/
+make clean
+make menuconfig KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-mmb-can
+
+echo -e 
+echo -e 
+echo -e 
+
+mcus=$(find /dev/serial/by-id/* 2>/dev/null)
+
+for mcu in ${mcus}; do
+    mcu_list+=("${mcu}")
+done
+
+  ### list all mcus
+  for mcu in "${mcu_list[@]}"; do
+    i=$(( i + 1 ))
+    mcu=$(echo "${mcu}" | rev | cut -d"/" -f1 | rev)
+    echo -e " ● MCU #${i}: ${cyan}${mcu}${white}"
+  done
+
+
+read -p "Input flash device: " flash_dev
+
+
+echo -e "flashing: " ${mcu_list[$flash_dev-1]}
+
+make clean
+make
+
+sudo service klipper stop
+make flash FLASH_DEVICE=${mcu_list[$flash_dev-1]} KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-mmb-can
+sudo service klipper start
+
+
