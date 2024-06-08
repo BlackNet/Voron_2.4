9d47ce965354238bc3b211f1f3632dbed66ed200
Ed Street <edstreet@street-tek.com>
2024-06-08 00:02:19 -0400

    Autocommit from 2024-06-08 00:02:19
    Klipper on commit: 49c0ad6
    
    Moonraker on commit: 346a3d7
    
    Mainsail version: v2.11.2

--- klipper_menuconfigs/klipper_make_SB2209-can.sh
+++ klipper_menuconfigs/klipper_make_SB2209-can.sh
@@ -2,8 +2,8 @@ UUID="7c05a6ed647f"
 
 cd ~/klipper/
 make clean
-make menuconfig KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-ebb2209-rpi
-make
+#make menuconfig KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-ebb2209-rpi
+make KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-ebb2209-rpi
 
 echo -e 
 echo -e 
@@ -13,9 +13,7 @@ echo -e "flashing: SBB2209 @ UUID:" $UUID
 
 sudo service klipper stop
 
-#make flash FLASH_DEVICE=${mcu_list[$flash_dev-1]} KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-mmb-can
 python3 ~/Katapult/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u $UUID
-
 
 sudo service klipper start
 
