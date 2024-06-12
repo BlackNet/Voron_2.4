0c1c89a9337db530f9277b430d44b513faddf83b
Ed Street <edstreet@street-tek.com>
2024-06-11 21:39:16 -0400

    Autocommit from 2024-06-11 21:39:16
    Klipper on commit: 589bd64
    
    Moonraker on commit: 346a3d7
    
    Mainsail version: v2.11.2

---
+++ klipper_menuconfigs/klipper_make_mmb_katapult.sh
@@ -0,0 +1,23 @@
+
+cd ~/klipper/
+make clean
+make menuconfig KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-MMB-CAN-CANBOOT
+make KCONFIG_CONFIG=~//printer_data/config/klipper_menuconfigs/config-MMB-CAN-CANBOOT
+echo -e 
+echo -e 
+echo -e 
+
+##sudo service klipper stop
+
+# make sure the device is in DFU mode before running this!!
+sudo dfu-util -a 0 -D ~/Katapult/out/canboot.bin --dfuse-address 0x08000000:force:mass-erase:leave -d 0483:df11
+
+## follow up with flashing klipper via
+## python3 ~/katapult/scripts/flash_can.py -i can0 -f ~/klipper/out/klipper.bin -u $UUID
+
+# find UUID via 
+~/Katapult/scripts/flash_can.py -i can0 -q
+
+##sudo service klipper start
+
+
