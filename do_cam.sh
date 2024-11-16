a79de44abaae737db7e0181982fc61d72ea803ae
Ed Street <edstreet@street-tek.com>
2024-11-15 19:15:39 -0500

    Autocommit from 2024-11-15 19:15:39
    Klipper on commit: a340344
    
    Moonraker on commit: 4e00a07
    
    Mainsail version: v2.12.0

--- do_cam.sh
+++ do_cam.sh
@@ -10,7 +10,7 @@ v4l2-ctl -d /dev/video0 --set-ctrl=power_line_frequency=2
 v4l2-ctl -d /dev/video0 --set-ctrl=exposure_time_absolute=200
 v4l2-ctl -d /dev/video0 --set-ctrl=focus_absolute=40
 v4l2-ctl -d /dev/video0 --set-ctrl=white_balance_temperature=4395
-v4l2-ctl -d /dev/video0 --set-ctrl=tilt_absolute=-36000
+v4l2-ctl -d /dev/video0 --set-ctrl=tilt_absolute=36000
 v4l2-ctl -d /dev/video0 --set-ctrl=pan_absolute=3600
 v4l2-ctl -d /dev/video0 --set-ctrl=zoom_absolute=600
 v4l2-ctl -d /dev/video0 --set-ctrl=sharpness=200
